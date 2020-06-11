4233a2db3ecb5cd7f6c8697ad2b9f99ce205e536
gwathlobal <gwathlobal@yandex.ru>
2020-06-11 23:00:54 +0300

    Add eliminate satanists mission

---
+++ src/game-events/init-game-events-campaign.lisp
@@ -0,0 +1,61 @@
+(in-package :cotd)
+
+;;===========================
+;; WIN EVENTS
+;;===========================
+
+(set-game-event (make-instance 'game-event :id +game-event-campaign-satanists-move+
+                                           :descr-func #'(lambda ()
+                                                           (format nil "Each day, move satanists to a location nearby."))
+                                           :disabled nil
+                                           :on-check #'(lambda (world)
+                                                         (declare (ignore world))
+                                                         t)
+                                           :on-trigger #'(lambda (world)
+                                                           (let ((world-sector)
+                                                                 (cur-lair)
+                                                                 (lair-list-init ())
+                                                                 (lair-list-final ()))
+                                                             (loop for x from 0 below (array-dimension (cells (world-map world)) 0) do
+                                                               (loop for y from 0 below (array-dimension (cells (world-map world)) 1) do
+                                                                 (setf world-sector (aref (cells (world-map world)) x y))
+                                                                 (setf cur-lair (find +lm-feat-lair+ (feats world-sector) :key #'(lambda (a) (first a))))
+                                                                 (when cur-lair
+                                                                   (push (list cur-lair x y) lair-list-init))))
+
+                                                             (loop with select-sector = nil
+                                                                   for (lair x y) in lair-list-init
+                                                                   for avail-sectors = ()
+                                                                   do
+                                                                      (check-surroundings x y nil #'(lambda (dx dy)
+                                                                                                      (when (and (>= dx 0) (>= dy 0) (< dx (array-dimension (cells (world-map world)) 0)) (< dy (array-dimension (cells (world-map world)) 1))
+                                                                                                                 (setf world-sector (aref (cells (world-map world)) dx dy))
+                                                                                                                 (not (eq (wtype world-sector) :world-sector-normal-sea))
+                                                                                                                 (not (find +lm-feat-lair+ (feats world-sector) :key #'(lambda (a) (first a))))
+                                                                                                                 (not (find-if #'(lambda (a)
+                                                                                                                                   (if (and (= (second a) dx)
+                                                                                                                                            (= (third a) dx))
+                                                                                                                                     t
+                                                                                                                                     nil))
+                                                                                                                               lair-list-final)))
+                                                                                                        (push (list dx dy) avail-sectors))))
+                                                                      (when avail-sectors
+                                                                        (setf select-sector (nth (random (length avail-sectors)) avail-sectors))
+                                                                        (push (list lair (first select-sector) (second select-sector)) lair-list-final)))
+
+                                                             (loop for (lair x y) in lair-list-init do
+                                                               (setf world-sector (aref (cells (world-map world)) x y))
+                                                               (setf (feats world-sector) (remove +lm-feat-lair+ (feats world-sector) :key #'(lambda (a) (first a)))))
+
+                                                             (loop for (lair x y) in lair-list-final do
+                                                               (setf world-sector (aref (cells (world-map world)) x y))
+                                                               (push lair (feats world-sector))))
+                                                           )))
+
+
+
+
+
+
+
+
---
+++ src/game-events/init-game-events-eliminate-satanists.lisp
@@ -0,0 +1,156 @@
+(in-package :cotd)
+
+;;===========================
+;; WIN EVENTS
+;;===========================
+
+(set-game-event (make-instance 'game-event :id +game-event-eliminate-satanists-win-for-angels+
+                                           :descr-func #'(lambda ()
+                                                           (format nil "To win, destroy all demons & satanists in the district. To lose, have all angels killed."))
+                                           :disabled nil
+                                           :on-check #'(lambda (world)
+                                                         (if (or (and (= (loyal-faction *player*) +faction-type-angels+)
+                                                                      (> (total-angels (level world)) 0)
+                                                                      (zerop (total-demons (level world)))
+                                                                      (zerop (nth +faction-type-satanists+ (total-faction-list (level world)))))
+                                                                 (and (/= (loyal-faction *player*) +faction-type-angels+)
+                                                                      (> (total-angels (level world)) 0)
+                                                                      (zerop (total-demons (level world)))
+                                                                      (zerop (nth +faction-type-satanists+ (total-faction-list (level world))))
+                                                                      (zerop (nth +faction-type-military+ (total-faction-list (level world))))))
+                                                           t
+                                                           nil))
+                                           :on-trigger #'(lambda (world)
+                                                           (let ((if-player-won (if (or (= (loyal-faction *player*) +faction-type-angels+)
+                                                                                        (= (loyal-faction *player*) +faction-type-church+))
+                                                                                  t
+                                                                                  nil)))
+                                                             (trigger-game-over world
+                                                                                :final-str "Satanists eliminated."
+                                                                                :score (calculate-player-score (+ 1400 (if (not (mimic-id-list *player*))
+                                                                                                                         0
+                                                                                                                         (loop for mimic-id in (mimic-id-list *player*)
+                                                                                                                               for mimic = (get-mob-by-id mimic-id)
+                                                                                                                               with cur-score = 0
+                                                                                                                               when (not (eq mimic *player*))
+                                                                                                                                 do
+                                                                                                                                    (incf cur-score (cur-score mimic))
+                                                                                                                               finally (return cur-score)))))
+                                                                                :if-player-won if-player-won
+                                                                                :player-msg (if if-player-won
+                                                                                              (format nil "Congratulations! Your faction has won!~%")
+                                                                                              (format nil "Curses! Your faction has lost!~%"))
+                                                                                :game-over-type :game-over-angels-won))
+                                                           )))
+
+(set-game-event (make-instance 'game-event :id +game-event-eliminate-satanists-win-for-demons+
+                                           :descr-func #'(lambda ()
+                                                           (format nil "To win, destroy all military units in the district. To lose, have all satanists killed."))
+                                           :disabled nil
+                                           :on-check #'(lambda (world)
+                                                         (if (or (and (= (loyal-faction *player*) +faction-type-demons+)
+                                                                      (> (total-demons (level world)) 0)
+                                                                      (> (nth +faction-type-satanists+ (total-faction-list (level world))) 0)
+                                                                      (<= (nth +faction-type-military+ (total-faction-list (level world))) 0))
+                                                                 (and (/= (loyal-faction *player*) +faction-type-demons+)
+                                                                      (> (total-demons (level world)) 0)
+                                                                      (> (nth +faction-type-satanists+ (total-faction-list (level world))) 0)
+                                                                      (<= (nth +faction-type-military+ (total-faction-list (level world))) 0)))
+                                                             t
+                                                             nil))
+                                           :on-trigger #'(lambda (world)
+                                                           (let ((if-player-won (if (or (= (loyal-faction *player*) +faction-type-demons+)
+                                                                                        (= (loyal-faction *player*) +faction-type-satanists+))
+                                                                                  t
+                                                                                  nil)))
+                                                             (trigger-game-over world
+                                                                                :final-str "Satanists protected."
+                                                                                :score (calculate-player-score 1450)
+                                                                                :if-player-won if-player-won
+                                                                                :player-msg (if if-player-won
+                                                                                              (format nil "Congratulations! Your faction has won!~%")
+                                                                                              (format nil "Curses! Your faction has lost!~%"))
+                                                                                :game-over-type :game-over-demons-won))
+                                                           )))
+
+(set-game-event (make-instance 'game-event :id +game-event-eliminate-satanists-win-for-military+
+                                           :descr-func #'(lambda ()
+                                                           (format nil "To win, destroy all satanists in the district. To lose, have all military units killed."))
+                                           :disabled nil
+                                           :on-check #'(lambda (world)
+                                                         (if (and (> (nth +faction-type-military+ (total-faction-list (level world))) 0)
+                                                                  (zerop (nth +faction-type-satanists+ (total-faction-list (level world)))))
+                                                           t
+                                                           nil))
+                                           :on-trigger #'(lambda (world)
+                                                           (let ((if-player-won (if (= (loyal-faction *player*) +faction-type-military+)
+                                                                                  t
+                                                                                  nil)))
+                                                             (trigger-game-over world
+                                                                                :final-str "Satanists eliminated."
+                                                                                :score (calculate-player-score (+ 1500 (* 10 (total-humans (level world)))))
+                                                                                :if-player-won if-player-won
+                                                                                :player-msg (if if-player-won
+                                                                                              (format nil "Congratulations! Your faction has won!~%")
+                                                                                              (format nil "Curses! Your faction has lost!~%"))
+                                                                                :game-over-type :game-over-military-won))
+                                                           )))
+
+(set-game-event (make-instance 'game-event :id +game-event-eliminate-satanists-win-for-church+
+                                           :descr-func #'(lambda ()
+                                                           "To win, destroy all satanists and demons in the district. To lose, get all priests and angels killed.")
+                                           :disabled nil
+                                           :on-check #'(lambda (world)
+                                                         (if (and (= (loyal-faction *player*) +faction-type-church+)
+                                                                  (> (nth +faction-type-church+ (total-faction-list (level world))) 0)
+                                                                  (zerop (total-demons (level world)))
+                                                                  (zerop (nth +faction-type-satanists+ (total-faction-list (level world)))))
+                                                           t
+                                                           nil))
+                                           :on-trigger #'(lambda (world)
+                                                           (let ((if-player-won (if (or (= (loyal-faction *player*) +faction-type-angels+)
+                                                                                        (= (loyal-faction *player*) +faction-type-church+))
+                                                                                  t
+                                                                                  nil)))
+                                                             (trigger-game-over world
+                                                                                :final-str "Satanists eliminated."
+                                                                                :score (calculate-player-score (+ 1450 (if (not (mimic-id-list *player*))
+                                                                                                                         0
+                                                                                                                         (loop for mimic-id in (mimic-id-list *player*)
+                                                                                                                               for mimic = (get-mob-by-id mimic-id)
+                                                                                                                               with cur-score = 0
+                                                                                                                               when (not (eq mimic *player*))
+                                                                                                                                 do
+                                                                                                                                    (incf cur-score (cur-score mimic))
+                                                                                                                               finally (return cur-score)))))
+                                                                                :if-player-won if-player-won
+                                                                                :player-msg (if if-player-won
+                                                                                              (format nil "Congratulations! Your faction has won!~%")
+                                                                                              (format nil "Curses! Your faction has lost!~%"))
+                                                                                :game-over-type :game-over-church-won))
+                                                           )))
+
+(set-game-event (make-instance 'game-event :id +game-event-eliminate-satanists-win-for-satanists+
+                                           :descr-func #'(lambda ()
+                                                           (format nil "To win, destroy all military units in the district. To lose, have all satanists killed."))
+                                           :disabled nil
+                                           :on-check #'(lambda (world)
+                                                         (if (and (= (loyal-faction *player*) +faction-type-satanists+)
+                                                                  (> (nth +faction-type-satanists+ (total-faction-list (level world))) 0)
+                                                                  (zerop (nth +faction-type-military+ (total-faction-list (level world)))))
+                                                           t
+                                                           nil))
+                                           :on-trigger #'(lambda (world)
+                                                           (let ((if-player-won (if (or (= (loyal-faction *player*) +faction-type-demons+)
+                                                                                        (= (loyal-faction *player*) +faction-type-satanists+))
+                                                                                  t
+                                                                                  nil)))
+                                                             (trigger-game-over world
+                                                                                :final-str "Satanists protected."
+                                                                                :score (calculate-player-score 1450)
+                                                                                :if-player-won if-player-won
+                                                                                :player-msg (if if-player-won
+                                                                                              (format nil "Congratulations! Your faction has won!~%")
+                                                                                              (format nil "Curses! Your faction has lost!~%"))
+                                                                                :game-over-type :game-over-satanists-won))
+                                                           )))
